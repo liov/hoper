eaa199fdd7e7375cb3b172bf635d5008644b0381
liov <liov@maizuo.com>
2019-09-23 11:29:40 +0800

    feature: 抽离初始化

--- go/user/main.go
+++ go/user/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"github.com/liov/hoper/go/user/initialize"
+	"github.com/liov/hoper/go/initialize"
+	"github.com/liov/hoper/go/user/internal/config"
 	"github.com/liov/hoper/go/user/internal/dao"
 	"github.com/liov/hoper/go/utls/log"
 )
 
 func main() {
 	defer log.Sync()
-	initialize.Start()
+	initialize.Start(config.Conf)
 	log.Info(dao.Dao)
 }
