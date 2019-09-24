d23fac75792e27cae5cc7ac0aa470fb08754f7ff
liov <liov@maizuo.com>
2019-09-24 16:54:56 +0800

    feature: go-micro,gateway测试

--- go/v1/user/main.go
+++ go/v1/user/main.go
@@ -1,45 +1,16 @@
 package main
 
 import (
-	"context"
-	"net"
-
 	"github.com/liov/hoper/go/v1/initialize"
 	"github.com/liov/hoper/go/v1/initialize/dao"
-	pb "github.com/liov/hoper/go/v1/proto"
 	"github.com/liov/hoper/go/v1/user/internal/config"
 
 	"github.com/liov/hoper/go/v1/utils/log"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
-
-const (
-	port = ":50051"
-)
-
-// server is used to implement helloworld.GreeterServer.
-type server struct{}
-
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
-}
 
 func main() {
 	defer log.Sync()
 	defer dao.Dao.Close()
 	initialize.Start(config.Conf)
 	log.Info(*dao.Dao)
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
 }
