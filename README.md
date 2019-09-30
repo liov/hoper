a3d96afed8e83a91c0bbf22590f65cca21643850
liov <liov@maizuo.com>
2019-09-30 18:24:34 +0800

    feature: pingcap的rust grpc

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## 架构设想
 istio+k8s
+https://github.com/kubernetes/ingress-nginx 已经用openresty了，哈哈哈
---
+++ rust/v2/hello_v2/build.rs
@@ -0,0 +1,12 @@
+extern crate protoc_grpcio;
+
+fn main() {
+    let proto_root = "../../../protobuf";
+    println!("cargo:rerun-if-changed={}", proto_root);
+    protoc_grpcio::compile_grpc_protos(
+        &["../../../protobuf/helloworld.proto"],
+        &[proto_root],
+        &"protobuf",
+        None
+    ).expect("Failed to compile gRPC definitions!");
+}
