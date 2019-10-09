dde5829a07a7cb2854b3006835f8032975524b1b
liov <liov@maizuo.com>
2019-10-09 16:29:24 +0800

    bug: wsl2中编译没问题，但是wsl2网络雏形问题

--- rust/v2/hello_v2/src/log_util.rs
+++ rust/v2/hello_v2/src/log_util.rs
@@ -1,14 +1,8 @@
-extern crate slog;
-extern crate slog_async;
-extern crate slog_scope;
-extern crate slog_stdlog;
-extern crate slog_term;
-
 use std::fs::File;
 
-use self::slog::{Drain, Logger, OwnedKV};
-use self::slog_scope::GlobalLoggerGuard;
-use self::slog_term::{Decorator, FullFormat, PlainSyncDecorator, TermDecorator};
+use slog::{Drain, Logger, OwnedKV};
+use slog_scope::GlobalLoggerGuard;
+use slog_term::{Decorator, FullFormat, PlainSyncDecorator, TermDecorator};
 
 pub fn init_log(log_file: Option<String>) -> GlobalLoggerGuard {
     fn setup<D: Decorator + Send + 'static>(decorator: D) -> GlobalLoggerGuard {
