e82b6efcc6ed9a09cb7bf66646bda490d2d5fbe4
liov <liov@maizuo.com>
2019-10-14 15:47:53 +0800

    feature: 路由注册

--- go/v2/gateway/internal/controller/user.go
+++ go/v2/gateway/internal/controller/user.go
@@ -3,25 +3,23 @@ package controller
 import (
 	"net/http"
 
-	"github.com/kataras/iris"
 	"github.com/kataras/iris/context"
 	"github.com/liov/hoper/go/v2/gateway/internal/api/request"
 	"github.com/liov/hoper/go/v2/gateway/internal/service"
 )
 
 type UserController struct{
-	apiInfo
-	App *iris.Application
+	Controller
 }
 var userService = &service.UserService{}
 func (u *UserController) Add() {
 	u.api(
-		path("/user"),
+		path(""),
 		method(http.MethodPost),
 		describe("新增用户"),
 		auth("jyb"),
-		version(1),
-		handle(u.App,
+		version("1"),
+		handle(
 			func(ctx context.Context) {
 				var req request.AddUserReq
 				ctx.ReadJSON(&req)
@@ -34,12 +32,12 @@ func (u *UserController) Add() {
 
 func (u *UserController) Get() {
 	u.api(
-		path("/add/:id"),
+		path("/:id"),
 		method(http.MethodGet),
 		describe("get"),
 		auth("jyb"),
-		version(1),
-		handle(u.App,
+		version("1"),
+		handle(
 			func(ctx context.Context) {
 				ctx.Writef("返回")
 			}),
