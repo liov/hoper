a9c3a0d12d597f2666539e2bd0d9657eab1fb582
liov <liov@maizuo.com>
2019-10-17 11:52:41 +0800

    feature: 环境部署文档

--- build/kube.sh
+++ build/kube.sh
@@ -5,7 +5,7 @@ Username=liov
 name=liov
 context=dev
 # 配置一个名为 ${cluster} 的集群，并指定服务地址与根证书
-kubectl config set-cluster ${cluster} --server=${server} --certificate-authority=$HOME/.kube/${cluster}/${cluster}-ca.pem
+kubectl config set-cluster ${cluster} --server=${server} --certificate-authority=$1${cluster}-ca.pem
 # 设置一个用户为 ${Username} ，并配置访问的授权文件
 kubectl config set-credentials ${Username} --client-certificate=$1${name}.crt --client-key=$1${name}.pem --embed-certs=true
 # 设置一个名为 ${context} 使用 ${cluster} 集群与 ${Username} 用户的上下文，
