d869e948e4aff09ff25d22b2ddf153d4e2b2c0a9
liov <liov@maizuo.com>
2019-10-26 17:53:33 +0800

    feature: grpc

--- go/v2/gateway/internal/client/client.go
+++ go/v2/gateway/internal/client/client.go
@@ -1,3 +1,24 @@
 package client
 
-var UserClient  = GetUserClient()
+import (
+	"github.com/liov/hoper/go/v2/protobuf/user"
+	"google.golang.org/grpc"
+)
+
+func init() {
+	client,conn  := GetUserClient()
+	Client.UserClient = client
+	Client.conns = append(Client.conns,conn)
+}
+
+var Client client
+type client struct {
+	UserClient user.UserServiceClient
+	conns []*grpc.ClientConn
+}
+
+func (c *client) Close() {
+	for _,conn:=range c.conns{
+		conn.Close()
+	}
+}
--- go/v2/gateway/internal/service/user.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import "github.com/liov/hoper/go/v2/gateway/internal/api/request"
-
-type UserService struct {}
-
-func (u *UserService) Add(req *request.AddUserReq)  {
-
-}
