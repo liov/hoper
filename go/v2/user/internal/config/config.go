b1f7d45da05d7155dca24e10f45233bbef260a11
liov <liov@maizuo.com>
2019-11-01 18:00:50 +0800

    feature: grpc时间用string，数据库支持!!!

--- go/v2/user/internal/config/config.go
+++ go/v2/user/internal/config/config.go
@@ -1,29 +1,32 @@
 package config
 
 import (
+	"os"
 	"runtime"
 	"time"
 
+	"github.com/jinzhu/configor"
 	"github.com/liov/hoper/go/v2/initialize"
 	"github.com/liov/hoper/go/v2/utils/fs"
+	"github.com/liov/hoper/go/v2/utils/log"
 )
 
 type serverConfig struct {
-	Port     string
+	Port         string
 	ReadTimeout  time.Duration
 	WriteTimeout time.Duration
 
-	PassSalt        string
-	TokenMaxAge     int64
-	TokenSecret     string
-	JwtSecret       string
-	PageSize        int8
+	PassSalt    string
+	TokenMaxAge int64
+	TokenSecret string
+	JwtSecret   string
+	PageSize    int8
 
 	UploadDir      fs.Dir
-	UploadMaxSize  int
+	UploadMaxSize  int64
 	UploadAllowExt []string
 
-	LogSaveDir fs.Dir
+	LogSaveDir  fs.Dir
 	LogSaveName string
 	LogFileExt  string
 	TimeFormat  string
@@ -35,7 +38,7 @@ type serverConfig struct {
 	LuosimaoAPIKey    string
 
 	QrCodeSaveDir fs.Dir //二维码保存路径
-	PrefixUrl      string
+	PrefixUrl     string
 	FontSaveDir   fs.Dir //字体保存路径
 
 	CrawlerName string //爬虫
@@ -46,13 +49,9 @@ var DatabaseSettings = &DatabaseConfig{}
 var RedisSettings = &RedisConfig{}
 var MongoSettings = &MongoConfig{}*/
 
-
 type config struct {
 	//必须
-	Module string
-	Env    string
-	Volume fs.Dir
-
+	initialize.BasicConfig
 	//自定义的配置
 	Server serverConfig
 	//命令参数大于配置
@@ -76,6 +75,15 @@ func (d *duration) UnmarshalText(text []byte) error {
 }
 
 func (c *config) Custom() {
+	if c.Flag.Additional != ""{
+		err := configor.New(&configor.Config{Debug: c.Env != initialize.PRODUCT}).
+			Load(c, c.Flag.Additional)
+		if err != nil {
+			log.Errorf("配置错误: %v", err)
+			os.Exit(10)
+		}
+	}
+
 	if runtime.GOOS == "windows" {
 		c.Server.LuosimaoAPIKey = ""
 		if c.Flag.Password != "" {
@@ -90,5 +98,4 @@ func (c *config) Custom() {
 	c.Server.UploadMaxSize = c.Server.UploadMaxSize * 1024 * 1024
 	c.Server.ReadTimeout = c.Server.ReadTimeout * time.Second
 	c.Server.WriteTimeout = c.Server.WriteTimeout * time.Second
-	c.Redis.IdleTimeout = c.Redis.IdleTimeout * time.Second
 }
--- go/v2/user/internal/config/flag.go
+++ go/v2/user/internal/config/flag.go
@@ -5,9 +5,11 @@ import "flag"
 type flagValue struct {
 	Password string
 	MailPassword string
+	Additional string
 }
 
 func init()  {
 	flag.StringVar(&Conf.Flag.Password, "p", "", "password")
 	flag.StringVar(&Conf.Flag.MailPassword, "mp", "", "password")
+	flag.StringVar(&Conf.Flag.Additional,"a","","额外的配置")
 }
