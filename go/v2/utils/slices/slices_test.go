7d08c8a7374fad2da0958fb8eaf839a2f27cf57e
贾一饼 <liov@maizuo.com>
2019-11-15 16:55:37 +0800

    feature: 新证书

--- go/v2/utils/slices/slices_test.go
+++ go/v2/utils/slices/slices_test.go
@@ -2,6 +2,7 @@ package slices
 
 import (
 	"fmt"
+	"log"
 	"testing"
 )
 
@@ -11,4 +12,34 @@ func TestContains(t *testing.T) {
 	val3 := "d"
 	fmt.Println(StringContains(val1, val2))
 	fmt.Println(StringContains(val1, val3))
+}
+
+type Foo struct{
+	ID uint64
+	Str string
+}
+
+func (f *Foo) IsEqual(v interface{}) bool {
+	if f1,ok := v.(*Foo);ok {
+		if f1.ID == f.ID {
+			return true
+		}
+	}
+	return false
+}
+
+var _ Equal = &Foo{}
+
+func TestIsEqu(t *testing.T){
+	s1:= []Equal{
+		&Foo{1, "1"},
+		&Foo{2, "2"},
+		&Foo{3, "3"},
+	}
+	s2:= []Equal{
+		&Foo{4,"1"},
+		&Foo{5,"1"},
+		&Foo{6,"1"},
+	}
+	log.Println(IsCoincide(s1,s2))
 }
