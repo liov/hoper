299693a0ba5182dda82e083ef89fadc2e0d63976
贾一饼 <lby.i@qq.com>
2019-11-16 18:05:53 +0800

    feature: grpc返回错误处理

--- go/v2/user/internal/server/grpc.go
+++ go/v2/user/internal/server/grpc.go
@@ -8,6 +8,7 @@ import (
 	model "github.com/liov/hoper/go/v2/protobuf/user"
 	"github.com/liov/hoper/go/v2/user/internal/service"
 	"github.com/liov/hoper/go/v2/utils/log"
+	"github.com/liov/hoper/go/v2/utils/protobuf/response"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -19,22 +20,28 @@ func filter(
 ) (resp interface{}, err error) {
 	defer func() {
 		if r := recover(); r != nil {
-			log.Errorf("%v panic: %v",info,r)
+			log.Errorf("%v panic: %v", info, r)
+		}
+		if err != nil {
+			resp = response.AnyReply{
+				Code: 1000,
+				Data: nil,
+				Msg:  err.Error(),
+			}
 		}
 	}()
 
 	return handler(ctx, req)
 }
 
-func Grpc() *grpc.Server{
+func Grpc() *grpc.Server {
 	s := grpc.NewServer(
 		//filter应该在最后
-		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(grpc_validator.UnaryServerInterceptor(),filter)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(grpc_validator.UnaryServerInterceptor(), filter)),
 		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(grpc_validator.StreamServerInterceptor())),
-		)
+	)
 	model.RegisterUserServiceServer(s, &service.UserService{})
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
-	return  s
+	return s
 }
-
