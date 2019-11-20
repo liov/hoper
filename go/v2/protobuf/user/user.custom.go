716da2ae6119564db807bf2690dd200cd439dbac
贾一饼 <liov@maizuo.com>
2019-11-20 18:35:51 +0800

    feature: 去重util

--- go/v2/protobuf/user/user.custom.go
+++ go/v2/protobuf/user/user.custom.go
@@ -1,11 +1,1 @@
 package model
-
-import (
-	"encoding/json"
-
-	"github.com/gogo/protobuf/jsonpb"
-)
-
-func (m *User) MarshalJSONPB(*jsonpb.Marshaler) ([]byte, error) {
-	return json.Marshal(m)
-}
