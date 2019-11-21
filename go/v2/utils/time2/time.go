d1ab5c72c2cc1dac4234d8b290534a5e295f2693
liov <liov@maizuo.com>
2019-11-21 18:43:38 +0800

    feature:可以有外部option的时候编译，java需要解决的冲突是新编译的文件与已存在的包冲突

--- go/v2/utils/time2/time.go
+++ go/v2/utils/time2/time.go
@@ -1,6 +1,7 @@
 package time2
 
 import (
+	"os/exec"
 	"time"
 
 	"github.com/liov/hoper/go/v2/utils/log"
@@ -12,4 +13,15 @@ func Format(t time.Time) string {
 
 func TimeCost(start time.Time) {
 	log.Info(time.Since(start))
+}
+
+// 设置系统时间
+func SetUnixSysTime(t time.Time) {
+	cmd := exec.Command("date", "-s", t.Format("01/02/2006 15:04:05.999999999"))
+	cmd.Run()
+}
+
+func SyncHwTime() {
+	cmd := exec.Command("clock --systohc")
+	cmd.Run()
 }
