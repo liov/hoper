0835942150a793fff1901602849bf3d7c5e4ab00
贾一饼 <lby.i@qq.com>
2019-11-27 18:54:39 +0800

    feature: rust异步

--- similarC/rust/v2/async_test/src/bin/async.rs
+++ similarC/rust/v2/async_test/src/bin/async.rs
@@ -5,7 +5,4 @@ async fn say_hello() {
 #[async_std::main]
 async fn main() {
     say_hello().await;
-    let a = async { 1u8 };
-    let b = async { 2u8 };
-    assert_eq!(a.join(b).await, (1u8, 2u8));
 }
