33e3adeadd1700756ea75864a360d222a1109d2a
liov <lby.i@qq.com>
2019-11-30 22:15:30 +0800

    feature: bazel构建

---
+++ similarC/rust/v2/async_test/src/bin/fuse_terminated.rs
@@ -0,0 +1,45 @@
+use futures::{
+    executor::block_on,
+    future::{Fuse, FusedFuture, FutureExt},
+    stream::{FusedStream, Stream, empty, StreamExt},
+    pin_mut,
+    select,
+};
+
+fn main() {
+    block_on(run_loop(empty(), 6));
+}
+
+async fn get_new_num() -> u8 { /* ... */ 5 }
+
+async fn run_on_new_num(_: u8) { /* ... */ }
+
+async fn run_loop(
+    mut interval_timer: impl Stream<Item=()> + FusedStream + Unpin,
+    starting_num: u8,
+) {
+    let run_on_new_num_fut = run_on_new_num(starting_num).fuse();
+    let get_new_num_fut = Fuse::terminated();
+    pin_mut!(run_on_new_num_fut, get_new_num_fut);
+    loop {
+        select! {
+            () = interval_timer.select_next_some() => {
+                // The timer has elapsed. Start a new `get_new_num_fut`
+                // if one was not already running.
+                if get_new_num_fut.is_terminated() {
+                    get_new_num_fut.set(get_new_num().fuse());
+                }
+            },
+            new_num = get_new_num_fut => {
+                // A new number has arrived-- start a new `run_on_new_num_fut`,
+                // dropping the old one.
+                run_on_new_num_fut.set(run_on_new_num(new_num).fuse());
+            },
+            // Run the `run_on_new_num_fut`
+            () = run_on_new_num_fut => {},
+            // panic if everything completed, since the `interval_timer` should
+            // keep yielding values indefinitely.
+            complete => panic!("`interval_timer` completed unexpectedly"),
+        }
+    }
+}
