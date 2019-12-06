f8a04a632935ffd4c4f894a5095a547941513969
liov <lby.i@qq.com>
2019-12-06 14:59:05 +0800

    feature: api mock

--- similarC/go/v2/user/main.go
+++ similarC/go/v2/user/main.go
@@ -5,8 +5,8 @@ package main
 //go:generate protoc -I../../../../protobuf/ -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I$GOPATH/src/github.com/gogo/protobuf/protobuf  ../../../../protobuf/user/*.service.proto --grpc-gateway_out=logtostderr=true,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,Mgoogle/api/annotations.proto=github.com/gogo/googleapis/google/api,Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:../protobuf
 //go:generate protoc -I../../../../protobuf/ -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I$GOPATH/src/github.com/gogo/protobuf/protobuf  ../../../../protobuf/user/*.service.proto --swagger_out=logtostderr=true,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,Mgoogle/api/annotations.proto=github.com/gogo/googleapis/google/api,Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:../protobuf
 //go:generate protoc -I../../../../protobuf/ -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I$GOPATH/src/github.com/gogo/protobuf/protobuf  ../../../../protobuf/user/*.service.proto --govalidators_out=gogoimport=true,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,Mgoogle/api/annotations.proto=github.com/gogo/googleapis/google/api,Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:../protobuf
+//go:generate mockgen -destination ../protobuf/user/mock/user.mock.go -package mock github.com/liov/hoper/go/v2/protobuf/user UserServiceServer
 import (
-
 	"os/signal"
 	"syscall"
 
@@ -15,7 +15,6 @@ import (
 	"github.com/liov/hoper/go/v2/user/internal/dao"
 	"github.com/liov/hoper/go/v2/user/internal/server"
 )
-
 
 func main() {
 	defer initialize.Start(config.Conf, dao.Dao)()
