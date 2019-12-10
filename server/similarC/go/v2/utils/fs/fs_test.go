8b60c6657ee6b6e1f6942c84371847f9336917db
liov <lby.i@qq.com>
2019-12-10 17:47:12 +0800

    feat: 文件查找util

---
+++ server/similarC/go/v2/utils/fs/fs_test.go
@@ -0,0 +1,21 @@
+package fs
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/liov/hoper/go/v2/utils/log"
+)
+
+func TestFindFile(t *testing.T) {
+	path, err := FindFile("cmd")
+	if err != nil {
+		log.Fatal(err)
+	}
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(bytes))
+}
