3ac7892e29d4ad5c80edfea3e88a26198c2b9c96
贾一饼 <lby.i@qq.com>
2019-12-11 11:36:47 +0800

    feat: 多文件查找

--- server/similarC/go/v2/helloworld/main.go
+++ server/similarC/go/v2/helloworld/main.go
@@ -12,26 +12,26 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-//go:generate protoc -I../../../proto/ -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I$GOPATH/src/github.com/gogo/protobuf/protobuf  ../../../proto/*.proto --gogo_out=plugins=grpc,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,Mgoogle/api/annotations.proto=github.com/gogo/googleapis/google/api,Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:../protobuf
+//go:generate protoc -I../../../../../proto/ -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I$GOPATH/src/github.com/gogo/protobuf/protobuf  ../../../../../proto/*.proto --gogo_out=plugins=grpc,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,Mgoogle/api/annotations.proto=github.com/gogo/googleapis/google/api,Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:../protobuf
 
-func main()  {
+func main() {
 	lis, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	protobuf.RegisterGreeterServer(s,&Service{})
+	protobuf.RegisterGreeterServer(s, &Service{})
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
 
-type Service struct {}
+type Service struct{}
 
-func (*Service) SayHello(ctx context.Context, in *protobuf.HelloRequest) (*protobuf.HelloReply,error) {
-	t:= time.Now()
-	tt:= time2.Time2(t)
-	log.Println("请求： ",in,int64(tt),t.Unix())
+func (*Service) SayHello(ctx context.Context, in *protobuf.HelloRequest) (*protobuf.HelloReply, error) {
+	t := time.Now()
+	tt := time2.Time2(t)
+	log.Println("请求： ", in, int64(tt), t.Unix())
 	return &protobuf.HelloReply{Time: tt}, nil
 }
--- server/similarC/go/v2/utils/fs/fs.go
+++ server/similarC/go/v2/utils/fs/fs.go
@@ -31,13 +31,13 @@ func FindFile(path string) (string, error) {
 		return "", err
 	}
 	filepath1 := filepath.Join(wd, path)
-	if _, err = os.Stat(filepath1); os.IsExist(err) {
+	if _, err = os.Stat(filepath1); !os.IsNotExist(err) {
 		return filepath1, nil
 	}
 	if subFilepath := subDirFile(wd, path); subFilepath != "" {
 		return subFilepath, nil
 	}
-	if supFilepath := supDirFile(wd, path); supFilepath != "" {
+	if supFilepath := supDirFile(wd+"/", path); supFilepath != "" {
 		return supFilepath, nil
 	}
 	return "", errors.New("找不到文件")
@@ -51,6 +51,7 @@ func subDirFile(dir, path string) string {
 	for i := range fileInfos {
 		if fileInfos[i].IsDir() {
 			filepath1 := filepath.Join(dir, fileInfos[i].Name(), path)
+			log.Println("subDirFile", filepath1)
 			if _, err = os.Stat(filepath1); !os.IsNotExist(err) {
 				return filepath1
 			}
@@ -66,6 +67,7 @@ func supDirFile(dir, path string) string {
 		return ""
 	}
 	filepath1 := filepath.Join(dir, path)
+	log.Println("supDirFile", filepath1)
 	if _, err := os.Stat(filepath1); !os.IsNotExist(err) {
 		return filepath1
 	}
@@ -78,8 +80,84 @@ func supDirFile(dir, path string) string {
 			if fileInfos[i].Name() == dirName {
 				continue
 			}
-			subDirFile(filepath.Join(dir, fileInfos[i].Name()), path)
+			if subFilepath := subDirFile(filepath.Join(dir, fileInfos[i].Name()), path); subFilepath != "" {
+				return subFilepath
+			}
 		}
 	}
 	return supDirFile(dir, path)
+}
+
+func FindFiles(path string, deep int8, callback func(filepath string)) ([]string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	var files = make([]string, 0, 5)
+	filepath1 := filepath.Join(wd, path)
+	if _, err = os.Stat(filepath1); !os.IsNotExist(err) {
+		files = append(files, filepath1)
+	}
+
+	subDirFiles(wd, path, &files, deep, 0)
+	supDirFiles(wd, path, &files, deep, 0)
+	if len(files) == 0 {
+		return nil, errors.New("找不到文件")
+	}
+	for i := range files {
+		if callback != nil {
+			callback(files[i])
+		}
+	}
+	return files, nil
+}
+
+func subDirFiles(dir, path string, files *[]string, deep, step int8) {
+	step += 1
+	if step > deep {
+		return
+	}
+	fileInfos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Println(err)
+	}
+	for i := range fileInfos {
+		if fileInfos[i].IsDir() {
+			filepath1 := filepath.Join(dir, fileInfos[i].Name(), path)
+			log.Println("subDirFiles", filepath1, step)
+			if _, err = os.Stat(filepath1); !os.IsNotExist(err) {
+				*files = append(*files, filepath1)
+			}
+			subDirFiles(filepath.Join(dir, fileInfos[i].Name()), path, files, deep, step)
+		}
+	}
+}
+
+func supDirFiles(dir, path string, files *[]string, deep, step int8) {
+	step += 1
+	if step > deep {
+		return
+	}
+	dir, dirName := filepath.Split(dir[:len(dir)-1])
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return
+	}
+	filepath1 := filepath.Join(dir, path)
+	log.Println("supDirFile", filepath1, step)
+	if _, err := os.Stat(filepath1); !os.IsNotExist(err) {
+		*files = append(*files, filepath1)
+	}
+	fileInfos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Println(err)
+	}
+	for i := range fileInfos {
+		if fileInfos[i].IsDir() {
+			if fileInfos[i].Name() == dirName {
+				continue
+			}
+			subDirFiles(filepath.Join(dir, fileInfos[i].Name()), path, files, deep, 0)
+		}
+	}
+	supDirFiles(dir, path, files, deep, step)
 }
