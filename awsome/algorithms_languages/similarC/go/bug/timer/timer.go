d9f8479255e78b60f04c52ad478ee2c812ebc9aa
贾一饼 <liov@maizuo.com>
2020-02-25 17:58:15 +0800

    bug: https://github.com/golang/go/issues/35394

--- awsome/algorithms_languages/similarC/go/bug/timer/timer.go
+++ awsome/algorithms_languages/similarC/go/bug/timer/timer.go
@@ -8,6 +8,10 @@ import "time"
 经过排查最终锁定在timer.go ReSet，不确定是第三方包调用的问题还是go timer本身的问题
 搜索发现一个大佬提出的相同的issues,不过他测试1.13没问题，服务器go版本是1.13.4发现的问题
 运行他给的bench多次也没出现问题，有待解决
+
+这是一个并发重置定时器的问题，在go1.13.4之前都是不报告这个错误的，因此不代表之前没有这个错误，而是没有报
+https://github.com/golang/go/issues/35394#issuecomment-550488852
+
 */
 func main() {
 	t := time.NewTimer(time.Second)
