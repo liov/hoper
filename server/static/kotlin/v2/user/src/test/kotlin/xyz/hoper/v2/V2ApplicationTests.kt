e1569f95f2807dba46e106185afcecc356d76b76
liov <liov@maizuo.com>
2020-03-06 18:14:57 +0800

    todo: java->kotlin,js->ts

---
+++ server/static/kotlin/v2/user/src/test/kotlin/xyz/hoper/v2/V2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.hoper.v2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class V2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
