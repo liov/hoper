95ee7f6ebd26596d7425884ec0dfb6643369bbed
贾一饼 <liov@maizuo.com>
2020-03-27 12:33:39 +0800

    feat: 约定大于配置，依据环境启动

--- awsome/algorithms_languages/similarC/go/serilalization/marshaljson.go
+++ awsome/algorithms_languages/similarC/go/serilalization/marshaljson.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 )
 
 type Foo struct {
@@ -11,9 +12,16 @@ type Foo struct {
 }
 
 func main() {
+	log.SetFlags(15)
 	foo := Foo{1, "序列化"}
 	b, _ := json.Marshal(&foo)
 	fmt.Println(string(b))
+	var foo1 interface{}
+	err := json.Unmarshal(b, &foo1)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(foo1)
 }
 
 func (foo *Foo) MarshalJSON() ([]byte, error) {
