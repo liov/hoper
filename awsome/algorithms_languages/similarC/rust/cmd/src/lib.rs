8df2833051a9a206afa513d31feaf9ed1ed0d026
liov <lby.i@qq.com>
2020-05-01 22:57:07 +0800

    feat: rust iter test,移除rust "grpc"库版本的grpc实现

--- awsome/algorithms_languages/similarC/rust/cmd/src/lib.rs
+++ awsome/algorithms_languages/similarC/rust/cmd/src/lib.rs
@@ -1,1 +1,12 @@
-
+mod test{
+    #[test]
+    fn iter(){
+        let a =vec![1,2,3,4,5];
+        let b =vec![1,2,3,4,5];
+        let c: Vec<i32> = a.iter().
+            zip(b.iter().skip(1)).
+            map(|(x,y)|x+y).
+            collect();
+        println!("{:?}",c);
+    }
+}
