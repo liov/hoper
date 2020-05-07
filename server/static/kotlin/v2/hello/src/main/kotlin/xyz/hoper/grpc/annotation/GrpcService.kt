01c4188d0005fea59f1448f2ec9c585d0f207ffa
liov <liov@maizuo.com>
2020-05-07 11:09:03 +0800

    bug: module模式下,idea的短命令行写入文件有问题

--- server/static/kotlin/v2/hello/src/main/kotlin/xyz/hoper/grpc/annotation/GrpcService.kt
+++ server/static/kotlin/v2/hello/src/main/kotlin/xyz/hoper/grpc/annotation/GrpcService.kt
@@ -1,12 +1,9 @@
 package xyz.hoper.grpc.annotation
 
 import org.springframework.stereotype.Component
-import java.lang.annotation.Documented
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 
 @Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
 @Component
 annotation class GrpcService
---
+++ server/static/kotlin/v2/web/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module v.web {
+    requires spring.beans;
+    requires spring.context;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires vertx.core;
+    requires vertx.web;
+}
