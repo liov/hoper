4e5370620c35c5a3c127eb0643cccdbbe4ec1d90
liov <lby.i@qq.com>
2020-05-11 22:26:00 +0800

    feat: java module玩不来

--- awsome/algorithms_languages/similarJava/kotlin/jvm/src/main/java/module-info.java
+++ awsome/algorithms_languages/similarJava/kotlin/jvm/src/main/java/module-info.java
@@ -1,7 +1,8 @@
-module test {
+module jvm {
     requires jdk.unsupported;
     requires org.apache.commons.lang3;
     requires org.objenesis;
     requires java.base;
     requires netty.all;
+    requires kotlin.stdlib;
 }
