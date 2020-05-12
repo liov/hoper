0e56abcb9ee96c9dd889ef08159e791566c63805
贾一饼 <liov@maizuo.com>
2020-05-12 12:41:02 +0800

    feat: 测试集c，cpp合并目录

--- awsome/algorithms_languages/similarJava/kotlin/jvm/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module jvm {
-    requires jdk.unsupported;
-    requires org.apache.commons.lang3;
-    requires org.objenesis;
-    requires java.base;
-    requires netty.all;
-    requires kotlin.stdlib;
-}
