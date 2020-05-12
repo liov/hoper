8cdbb75ebb06e383e4170f699bae316f71abcaa6
贾一饼 <liov@maizuo.com>
2020-05-12 15:43:44 +0800

    feat: 解决gradle java module，轻易不要用module

---
+++ awsome/algorithms_languages/similarJava/kotlin/jvm/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "xyz.hoper"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+var junitJupiterEngineVersion = "5.4.0"
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.objenesis:objenesis:3.0.1")
+    implementation("org.apache.commons:commons-lang3:3.8.1")
+    compileOnly("org.projectlombok:lombok:1.18.12")
+    annotationProcessor("org.projectlombok:lombok:1.18.12")
+    implementation("io.netty:netty-all:[5.0.0.Alpha2,)")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterEngineVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterEngineVersion")
+    implementation("org.jetbrains.kotlin:kotlin-script-runtime:1.3.61")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+var moduleName = "jvm"
+
+
+tasks {
+    compileJava{
+        inputs.property("moduleName", moduleName)
+        options.encoding = "UTF-8"
+        options.compilerArgs = listOf(
+          "--add-opens=java.base/jdk.internal.misc=jvm",
+          "--add-exports=java.base/jdk.internal.misc=jvm",
+          "--module-path", classpath.asPath
+        )
+        classpath = files()
+    }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+}
