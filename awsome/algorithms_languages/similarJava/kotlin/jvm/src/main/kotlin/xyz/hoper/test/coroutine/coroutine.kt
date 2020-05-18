20cf166021ab7f08989874ad88cd813bd943181c
贾一饼 <liov@maizuo.com>
2020-05-18 18:07:44 +0800

    feat: kotlin 协程 异步

--- awsome/algorithms_languages/similarJava/kotlin/jvm/src/main/kotlin/xyz/hoper/test/coroutine/coroutine.kt
+++ awsome/algorithms_languages/similarJava/kotlin/jvm/src/main/kotlin/xyz/hoper/test/coroutine/coroutine.kt
@@ -1,4 +1,4 @@
-package xyz.hoper.test
+package xyz.hoper.test.coroutine
 
 import kotlinx.coroutines.*
 
@@ -20,6 +20,18 @@ fun main() = runBlocking { // this: CoroutineScope
   }
 
   println("Coroutine scope is over") // 这一行在内嵌 launch 执行完毕后才输出
+  //取消协成
+  val job = launch {
+    repeat(1000) { i ->
+      println("job: I'm sleeping $i ...")
+      delay(500L)
+    }
+  }
+  delay(1300L) // 延迟一段时间
+  println("main: I'm tired of waiting!")
+  job.cancel() // 取消该作业
+  job.join() // 等待作业执行结束
+  println("main: Now I can quit.")
 }
 
 suspend fun doWorld() {
