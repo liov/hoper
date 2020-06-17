687fbfa8bd46741940b73ad5d996611fb619cb3c
贾一饼 <liov@maizuo.com>
2020-06-17 18:00:38 +0800

    feat: ts interface

--- awsome/algorithms_languages/similarJs/ts&js/ts/interface.ts
+++ awsome/algorithms_languages/similarJs/ts&js/ts/interface.ts
@@ -25,3 +25,43 @@ let cardPicker = deck.createCardPicker();
 let pickedCard = cardPicker();
 
 console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+interface ClockConstructor {
+    new (hour: number, minute: number): ClockInterface;
+}
+
+interface ClockInterface {
+    tick(): void;
+}
+
+function createClock(
+    ctor: ClockConstructor,
+    hour: number,
+    minute: number
+): ClockInterface {
+    return new ctor(hour, minute);
+}
+
+class DigitalClock implements ClockInterface {
+    constructor(h: number, m: number) {}
+    tick() {
+        console.log("beep beep");
+    }
+}
+
+class AnalogClock implements ClockInterface {
+    constructor(h: number, m: number) {}
+    tick() {
+        console.log("tick tock");
+    }
+}
+
+let digital = createClock(DigitalClock, 12, 17);
+let analog = createClock(AnalogClock, 7, 32);
+//class expressions
+const Clock: ClockConstructor = class Clock implements ClockInterface {
+    constructor(h: number, m: number) {}
+    tick() {
+        console.log("beep beep");
+    }
+};
