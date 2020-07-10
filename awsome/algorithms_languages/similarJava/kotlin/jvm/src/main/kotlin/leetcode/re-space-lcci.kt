91c2f5db31fd349a2f8e8711b922daaf776cee2d
liov <liov@maizuo.com>
2020-07-10 18:53:09 +0800

    feat: 基于生态，熟悉程度,目前是不会用c#开发web了

--- awsome/algorithms_languages/similarJava/kotlin/jvm/src/main/kotlin/leetcode/re-space-lcci.kt
+++ awsome/algorithms_languages/similarJava/kotlin/jvm/src/main/kotlin/leetcode/re-space-lcci.kt
@@ -25,103 +25,50 @@ dictionary中总字符数不超过 150000。
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 //前缀树，hash
+//考虑转移方程，每次转移的时候我们考虑第 j(j≤i) 个到第 i 个字符组成的子串sentence[j−1⋯i−1] （注意字符串下标从 0 开始）是否能在词典中找到，如果能找到的话按照定义转移方程即为
+//dp[i]=min(dp[i],dp[j−1])
+//
+//否则没有找到的话我们可以复用 dp[i−1] 的状态再加上当前未被识别的第 i 个字符，因此此时 dp 值为
+
+//dp[i]=dp[i−1]+1
+
 fun respace(dictionary: Array<String>, sentence: String): Int {
   val tree = TrieTree(dictionary)
-  var tmp = tree.root
-  var nextCount = 0
-  var count = sentence.length
-  var tmpCount = 0
-  var i = 0
-  while (i < sentence.length) {
-    if (tmp.child == null) {
-      tmpCount = 0
-      tmp = tree.root
-      continue
+
+  val dp = IntArray(sentence.length + 1) { Int.MAX_VALUE }
+  dp[0] = 0
+  for (i in 1..sentence.length) {
+    dp[i] = dp[i - 1] + 1
+    var tmp = tree.root
+    for (j in i downTo 1) {
+      val t = sentence[j - 1] - 'a'
+      if (tmp.child == null) break
+      else if (tmp.child!![t].isWord) dp[i] = kotlin.math.min(dp[i], dp[j - 1])
+      if (dp[i] == 0) break
+      tmp = tmp.child!![t]
     }
-    if (tmp.child!![26].exists) {
-      nextCount = tmpCount
-    }
-    tmpCount++
-    if (tmp.child!![sentence[i] - 'a'].exists) tmp = tmp.child!![sentence[i] - 'a']
-    else {
-      if (nextCount != 0) {
-        count -= nextCount
-        nextCount = 0
-      }
-      if (tmp != tree.root) {
-        tmp = tree.root
-        i--
-      }
-      tmpCount = 0
-    }
-    i++
   }
-  if (tmp.child!![26].exists) {
-    count -= tmpCount
-  }
-  return count
+  return dp.last()
 }
 
 class TrieTree {
-  val root = TrieTreeNode('\u0000', false)
+  val root = TrieTreeNode(false)
 
   constructor(dictionary: Array<String>) {
     for (i in dictionary.indices) insert(dictionary[i])
   }
 
-  fun insert(s: String) {
+  private fun insert(s: String) {
     var tmp = root
-    for (i in s.indices) {
-      tmp = tmp.insert(s[i])
-    }
-    tmp.insert('a' + 26)
+    for (i in s.length - 1 downTo 0) tmp = tmp.insert(s[i])
+    tmp.isWord = true
   }
 }
 
-class TrieTreeNode(val c: Char, var exists: Boolean) {
+class TrieTreeNode(var isWord: Boolean) {
   var child: Array<TrieTreeNode>? = null
   fun insert(c: Char): TrieTreeNode {
-    if (child == null) child = Array(27) { i -> TrieTreeNode('a' + i, false) }
-    if (!child!![c - 'a'].exists) child!![c - 'a'].exists = true
+    if (child == null) child = Array(26) { TrieTreeNode(false) }
     return child!![c - 'a']
   }
-}
-
-internal class Trie {
-  var root: TrieNode
-
-  // 将单词倒序插入字典树
-  fun insert(word: String) {
-    var cur: TrieNode? = root
-    for (i in word.length - 1 downTo 0) {
-      val c = word[i] - 'a'
-      if (cur!!.children[c] == null) {
-        cur.children[c] = TrieNode()
-      }
-      cur = cur.children[c]
-    }
-    cur!!.isWord = true
-  }
-
-  // 找到 sentence 中以 endPos 为结尾的单词，返回这些单词的开头下标。
-  fun search(sentence: String, endPos: Int): List<Int> {
-    val indices: ArrayList<Int> = ArrayList()
-    var cur: TrieNode? = root
-    for (i in endPos downTo 0) {
-      val c = sentence[i] - 'a'
-      if (cur!!.children[c] == null) break
-      cur = cur.children[c]
-      if (cur!!.isWord) indices.add(i)
-    }
-    return indices
-  }
-
-  init {
-    root = TrieNode()
-  }
-}
-
-internal class TrieNode {
-  var isWord = false
-  var children = arrayOfNulls<TrieNode>(26)
 }
