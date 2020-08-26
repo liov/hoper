ff84464df1bf13a08f3f60590fde7080e3b2c74f
liov <liov@maizuo.com>
2020-08-26 17:45:08 +0800

    feat: appbar

--- client/flutter/lib/service/moment.dart
+++ client/flutter/lib/service/moment.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:app/model/moment.dart';
+import 'package:app/util/dio.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'moment.g.dart';
@@ -14,4 +17,18 @@ class MomentListResponse {
   factory MomentListResponse.fromJson(Map<String, dynamic> json) => _$MomentListResponseFromJson(json);
 
   Map<String, dynamic> toJson() => _$MomentListResponseToJson(this);
+}
+
+Future<MomentListResponse> getMomentList(int pageNo,pageSize) async{
+  var api = '/moment?page=$pageNo&pageSize=$pageSize';
+
+  try {
+    var response = await httpClient().get(api);
+    if (response.statusCode == HttpStatus.ok) {
+      return MomentListResponse.fromJson(response.data);
+    }
+  } catch (exception) {
+
+  }
+  return null;
 }
