5475056fdf44ece99904b8c919af17ef45c2e64f
贾一饼 <liov@maizuo.com>
2020-09-10 15:57:55 +0800

    feat: grpc v2

--- server/go/v2/protobuf/generate.go
+++ server/go/v2/protobuf/generate.go
@@ -6,10 +6,10 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"runtime"
-	"strconv"
 	"strings"
 	"text/template"
+
+	"github.com/liov/hoper/go/v2/utils/os/cmd"
 )
 
 /*
@@ -20,49 +20,55 @@ import (
 
 func main() { run() }
 
+const goOut = "go-patch_out=plugin=go,paths=source_relative"
+const grpcOut = "go-patch_out=plugin=go-grpc,paths=source_relative"
+const enumOut = "enum_out=plugins=grpc"
+const gatewayOut = "grpc-gateway_out=logtostderr=true"
+const openapiv2Out = "openapiv2_out=logtostderr=true"
+const govalidatorsOut = "govalidators_out=gogoimport=true"
+
 var gengql = true
 var files = map[string][]string{
-	"/utils/empty/*.gen.proto":      {"gogo_out=plugins=grpc"},
-	"/utils/errorcode/errrep.proto": {"gogo_out=plugins=grpc"},
-	"/utils/errorcode/*enum.proto":  {"enum_out=plugins=grpc"},
-	"/utils/actor/message/*.proto":  {"gogo_out=plugins=grpc"},
-	"/utils/response/*.gen.proto":   {"gogo_out=plugins=grpc"},
-	"/utils/oauth/*.gen.proto":      {"gogo_out=plugins=grpc"},
-	"/utils/proto/gogo/*.gen.proto": {"gogo_out=plugins=grpc"},
+	"/utils/empty/*.gen.proto":      {goOut, grpcOut},
+	"/utils/errorcode/errrep.proto": {goOut},
+	"/utils/errorcode/*enum.proto":  {enumOut},
+	"/utils/actor/message/*.proto":  {goOut},
+	"/utils/response/*.gen.proto":   {goOut},
+	"/utils/oauth/*.gen.proto":      {grpcOut},
+	"/utils/proto/gogo/*.gen.proto": {grpcOut},
 	"/utils/proto/go/*.gen.proto":   {"go_out=plugins=grpc"},
-	"/user/*service.proto": {"gogo_out=plugins=grpc",
-		"grpc-gateway_out=logtostderr=true",
-		"openapiv2_out=logtostderr=true",
-		"govalidators_out=gogoimport=true",
+	"/user/*service.proto": {goOut,
+		gatewayOut,
+		openapiv2Out,
+		govalidatorsOut,
 		"gqlgen_out=gogoimport=false,paths=source_relative",
 		//"gqlgencfg_out=paths=source_relative",
 		"graphql_out=paths=source_relative"},
-	"/user/*model.proto": {"gogo_out=plugins=grpc"},
-	"/user/*enum.proto":  {"enum_out=plugins=grpc"},
-	"/note/*service.proto": {"gogo_out=plugins=grpc",
-		"grpc-gateway_out=logtostderr=true",
-		"openapiv2_out=logtostderr=true",
-		"govalidators_out=gogoimport=true"},
-	"/note/*model.proto": {"gogo_out=plugins=grpc"},
+	"/user/*model.proto": {goOut},
+	"/user/*enum.proto":  {enumOut},
+	"/note/*service.proto": {goOut,
+		gatewayOut,
+		openapiv2Out,
+		govalidatorsOut},
+	"/note/*model.proto": {goOut, grpcOut},
 }
 
 var proto = flag.String("proto", "../../../proto", "proto路径")
 
-var env = []string{
-	"GOARCH=" + runtime.GOARCH,
-	"GOOS=" + runtime.GOOS,
-}
-
 func run() {
 	pwd, _ := os.Getwd()
 	*proto = pwd + "/" + *proto
+	goList := `go list -m -f {{.Dir}} `
+	gateway, _ := cmd.CMD(goList + "github.com/grpc-ecosystem/grpc-gateway/v2")
+	protopatch, _ := cmd.CMD(goList + "github.com/alta/protopatch")
+	protobuf, _ := cmd.CMD(goList + "google.golang.org/protobuf")
 	path := os.Getenv("GOPATH")
-	include := "-I" + *proto + " -I" + path + "/src -I" + path + "/src/github.com/grpc-ecosystem/grpc-gateway -I" + path + "/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I" + path + "/src/github.com/gogo/protobuf/protobuf "
+	include := "-I" + *proto + " -I" + gateway + " -I" + gateway + "/third_party/googleapis -I" + protopatch + " -I" + protobuf + " -I" + path + "/src"
 
 	var gqlgen []string
 	for k, v := range files {
 		for _, plugin := range v {
-			arg := "protoc " + include + *proto + k + " --" + plugin + ",Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,Mgoogle/api/annotations.proto=github.com/gogo/googleapis/google/api,Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:" + pwd + "/protobuf"
+			arg := "protoc " + include + " " + *proto + k + " --" + plugin + ":" + pwd + "/protobuf"
 			if strings.HasPrefix(plugin, "openapiv2_out") {
 				arg = arg + "/api"
 			}
@@ -80,7 +86,8 @@ func run() {
 			if strings.HasPrefix(k, "/utils/proto/gogo/") {
 				arg = "protoc -I" + *proto + " " + *proto + k + " --gogo_out=plugins=grpc,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor:" + pwd + "/protobuf"
 			}
-			words := split(arg)
+			log.Println(arg)
+			words := cmd.Split(arg)
 			cmd := exec.Command(words[0], words[1:]...)
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
@@ -107,7 +114,7 @@ func run() {
 				}
 				t.Execute(file, fileInfos[i].Name())
 				file.Close()
-				words := split(`gqlgen --verbose --config ` + config)
+				words := cmd.Split(`gqlgen --verbose --config ` + config)
 				cmd := exec.Command(words[0], words[1:]...)
 				cmd.Stdout = os.Stdout
 				cmd.Stderr = os.Stderr
@@ -118,72 +125,13 @@ func run() {
 	os.Chdir(pwd)
 
 	for i := range gqlgen {
-		words := split(gqlgen[i])
+		words := cmd.Split(gqlgen[i])
 		cmd := exec.Command(words[0], words[1:]...)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		cmd.Run()
 	}
 
-}
-
-func split(line string) []string {
-	var words []string
-Words:
-	for {
-		line = strings.TrimLeft(line, " \t")
-		if len(line) == 0 {
-			break
-		}
-		if line[0] == '"' {
-			for i := 1; i < len(line); i++ {
-				c := line[i] // Only looking for ASCII so this is OK.
-				switch c {
-				case '\\':
-					if i+1 == len(line) {
-						log.Panic("bad backslash")
-					}
-					i++ // Absorb next byte (If it's a multibyte we'll get an error in Unquote).
-				case '"':
-					word, err := strconv.Unquote(line[0 : i+1])
-					if err != nil {
-						log.Panic("bad quoted string")
-					}
-					words = append(words, word)
-					line = line[i+1:]
-					// Check the next character is space or end of line.
-					if len(line) > 0 && line[0] != ' ' && line[0] != '\t' {
-						log.Panic("expect space after quoted argument")
-					}
-					continue Words
-				}
-			}
-			log.Panic("mismatched quoted string")
-		}
-		i := strings.IndexAny(line, " \t")
-		if i < 0 {
-			i = len(line)
-		}
-		words = append(words, line[0:i])
-		line = line[i:]
-	}
-	// Substitute command if required.
-
-	// Substitute environment variables.
-	for i, word := range words {
-		words[i] = os.Expand(word, expandVar)
-	}
-	return words
-}
-
-func expandVar(word string) string {
-	w := word + "="
-	for _, e := range env {
-		if strings.HasPrefix(e, w) {
-			return e[len(w):]
-		}
-	}
-	return os.Getenv(word)
 }
 
 const ymlTpl = `schema:
