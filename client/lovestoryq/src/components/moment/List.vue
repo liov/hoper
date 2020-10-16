158f4f5d142c42e80dbab4845cad6fca44e58033
贾一饼 <lby.i@qq.com>
2020-10-16 18:13:49 +0800

    feat: 瞬间 登录

--- client/lovestoryq/src/components/moment/List.vue
+++ client/lovestoryq/src/components/moment/List.vue
@@ -1,25 +1,101 @@
 <template>
   <div>
-
+    <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell v-for="(item, index) in list" :key="index">
+        <template #default>
+          <van-skeleton title avatar :row="3" :loading="loading">
+            <div class="moment" v-if="show">
+              <img :src="item.user.avatar_url" />
+              <div class="content">
+                <span>{{ item.user.name }}</span>
+                <span>{{ item.created_at }}</span>
+                <div class="van-multi-ellipsis--l3">
+                  {{ item.content }}
+                </div>
+              </div>
+            </div>
+          </van-skeleton>
+        </template>
+      </van-cell>
+    </van-list>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import {Toast} from "vant";
+import axios from "axios";
 
 @Options({
-  components: {
-  }
+  components: {}
 })
 export default class MomentList extends Vue {
   active = 0;
-  onChange(index:number){
-    Toast({message:index})
+  list: any[] = [];
+  loading = false;
+  finished = false;
+  show = false;
+  pageNo = 0;
+  pageSize = 10;
+
+  mounted() {
+    if (this.list.length == 0) {
+      const list: any[] = [];
+      for (let i = 0; i < 5; i++) {
+        list.push({ id: i });
+      }
+      this.list = list;
+    }
+  }
+
+  async onLoad() {
+    // 异步更新数据
+    const res = await axios.get(
+      `/api/moment?pageNo=${this.pageNo}&pageSize=${this.pageSize}`
+    );
+    if (this.pageNo == 0) {
+      this.list = res.data.data;
+    } else this.list = this.list.concat(res.data.data);
+    this.loading = false;
+    this.show = true;
+    this.pageNo++;
+    if (res.data.data.length < this.pageSize) {
+      this.finished = true;
+    }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.moment {
+  display: flex;
+  padding: 0 16px;
 
+  .content {
+    padding-top: 6px;
+
+    h3 {
+      margin: 0;
+      font-size: 18px;
+      line-height: 20px;
+    }
+
+    .van-multi-ellipsis--l3 {
+      margin: 13px 0 0;
+      font-size: 14px;
+      line-height: 20px;
+    }
+  }
+
+  img {
+    flex-shrink: 0;
+    width: 32px;
+    height: 32px;
+    margin-right: 16px;
+  }
+}
 </style>
--- client/lovestoryq/src/main.ts
+++ client/lovestoryq/src/main.ts
@@ -3,7 +3,26 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import { Col, Row, NavBar, Tabbar, TabbarItem, Tab, Tabs, Icon  } from "vant";
+import "@/plugin/axios";
+import {
+  Col,
+  Row,
+  NavBar,
+  Tabbar,
+  TabbarItem,
+  Tab,
+  Tabs,
+  Icon,
+  List,
+  Skeleton,
+  Cell,
+  CellGroup,
+  Toast,
+  Notify,
+  Form,
+  Field,
+  Button
+} from "vant";
 
 createApp(App)
   .use(store)
@@ -16,4 +35,13 @@ createApp(App)
   .use(Tab)
   .use(Tabs)
   .use(Icon)
+  .use(List)
+  .use(Skeleton)
+  .use(Cell)
+  .use(CellGroup)
+  .use(Toast)
+  .use(Notify)
+  .use(Form)
+  .use(Field)
+  .use(Button)
   .mount("#app");
