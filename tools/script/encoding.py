dd888c976d41a483d8cf4a6bd55f07a6068e2a7e
贾一饼 <lby.i@qq.com>
2020-11-20 18:28:30 +0800

    feat: 爬虫

---
+++ tools/script/encoding.py
@@ -0,0 +1,5 @@
+import base64
+
+att = base64.b64decode("MjMwNzQyNXwzYmM3MjNiZXwxNjA1ODQwMzU1fDg5NTFTaEl"
+                       "aSkMwL3BSMjIwTm1KTEpOSWhHeFQ3WTRFcWpnMzF5eTE5TWFpNXpR").decode("utf-8")
+print(att)
