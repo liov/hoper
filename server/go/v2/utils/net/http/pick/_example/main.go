9a585e263c923b20aad73ac11beb52bb6de81eeb
贾一饼 <lby.i@qq.com>
2021-01-22 18:46:39 +0800

    feat: example

--- server/go/v2/utils/net/http/pick/_example/main.go
+++ server/go/v2/utils/net/http/pick/_example/main.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/liov/hoper/go/v2/utils/net/http/pick"
+	"github.com/liov/hoper/go/v2/utils/net/http/pick/_example/service"
 	_ "github.com/liov/hoper/go/v2/utils/net/http/pick/_example/service"
 )
 
 func main() {
 	router := pick.New(false, "httptpl")
+	pick.RegisterService(&service.UserService{}, &service.TestService{}, &service.StaticService{})
 	router.ServeFiles("/static", "E:/")
 	log.Println("visit http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
