eb033750399af104de3f70461b4479f3420416d2
贾一饼 <lby.i@qq.com>
2022-05-11 18:11:47 +0800

    feat: v2ray

--- build/shell/git_auth.sh
+++ build/shell/git_auth.sh
@@ -15,3 +15,14 @@ then
     GIT_AUTHOR_EMAIL="newaddr";
 fi
 ' -- --all
+
+
+git config --global http.proxy 'socks5://127.0.0.1:1080'
+
+git config --global https.proxy 'socks5://127.0.0.1:1080'
+
+vi ~/.gitconfig
+[http]
+proxy = socks5://127.0.0.1:2080
+[https]
+proxy = socks5://127.0.0.1:2080
---
+++ build/shell/net.sh
@@ -0,0 +1,4 @@
+
+netstat -ntulp
+netstat -ntulp | grep 9090
+lsof -i:9090
