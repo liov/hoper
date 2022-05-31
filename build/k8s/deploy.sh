d4757e25f29c2a9ea5ba9b857cf38f5563484432
贾一饼 <lby.i@qq.com>
2022-05-31 16:07:44 +0800

    fix: 远程发布

--- build/k8s/deploy.sh
+++ build/k8s/deploy.sh
@@ -23,6 +23,7 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
 minikube start --driver=none  --image-repository=registry.cn-hangzhou.aliyuncs.com/google_containers --extra-config=apiserver.service-node-port-range=1-39999  --extra-config=kube-proxy.mode=ipvs --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.bind-address=0.0.0.0 --extra-config=controller-manager.bind-address=0.0.0.0 --bootstrapper=kubeadm
 
+cp -r /var/lib/minikube/certs/etcd /root/certs/ && chmod 666 /root/certs/etcd/server.key
 minikube addons enable dashboard
 minikube addons enable logviewer
 minikube addons enable efk
