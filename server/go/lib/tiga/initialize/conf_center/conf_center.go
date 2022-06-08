fe19d04b8e4efa2b5f01f7754541681561e94fba
贾一饼 <lby.i@qq.com>
2022-06-08 16:24:11 +0800

    fix: nacos

--- server/go/lib/tiga/initialize/conf_center/conf_center.go
+++ server/go/lib/tiga/initialize/conf_center/conf_center.go
@@ -3,52 +3,46 @@ package conf_center
 import (
 	"github.com/actliboy/hoper/server/go/lib/tiga/initialize/conf_center/local"
 	"github.com/actliboy/hoper/server/go/lib/tiga/initialize/conf_center/nacos"
-	local2 "github.com/actliboy/hoper/server/go/lib/utils/configor/local"
+	v2 "github.com/actliboy/hoper/server/go/lib/tiga/initialize/conf_center/nacos/v2"
+)
+
+const (
+	Local   = "local"
+	Nacos   = "nacos"
+	Nacosv2 = "nacosv2"
 )
 
 type ConfigCenter interface {
 	HandleConfig(func([]byte)) error
 }
 
-type ConfigCenterConfig struct {
-	Watch bool
-	Nacos *nacos.Nacos
-	Local *local.Local
+type ConfigCenterEnvConfig struct {
+	ConfigType string
+	Watch      bool
+	Nacos      *nacos.Nacos
+	Nacosv2    *v2.Nacos
+	Local      *local.Local
 	/*	Etcd   *etcd.Etcd
 		Apollo *apollo.Apollo*/
 }
 
-type ConfigCenterEnvConfig struct {
-	NacosTenant string
-	//本地配置，特定文件名,启用文件搜寻查找
-	LocalConfigName string
-	EtcdKey         string
-}
-
-func (c *ConfigCenterConfig) ConfigCenter(ccec ConfigCenterEnvConfig, model string, debug bool) ConfigCenter {
-	if c.Nacos != nil && ccec.NacosTenant != "" {
-		c.Nacos.DataId = model
-		c.Nacos.Tenant = ccec.NacosTenant
+func (c *ConfigCenterEnvConfig) ConfigCenter(model string, debug bool) ConfigCenter {
+	if c.ConfigType == Nacos && c.Nacos != nil {
 		c.Nacos.Watch = c.Watch
 		return c.Nacos
+	}
+
+	if c.ConfigType == Nacosv2 && c.Nacosv2 != nil {
+		return c.Nacosv2
 	}
 	/*	if c.Etcd != nil && ccec.EtcdKey != "" {
 		c.Etcd.Key = ccec.EtcdKey
 		c.Etcd.Watch = c.Watch
 		return c.Etcd
 	}*/
-	if ccec.LocalConfigName != "" {
-		if c.Local != nil {
-			c.Local.LocalConfigName = ccec.LocalConfigName
-			c.Local.AutoReload = c.Watch
-			return c.Local
-		} else {
-			return &local.Local{Config: local2.Config{
-				Debug:      debug,
-				AutoReload: c.Watch,
-			},
-				LocalConfigName: ccec.LocalConfigName}
-		}
+	if c.ConfigType == Local && c.Local != nil {
+		c.Local.AutoReload = c.Watch
+		return c.Local
 	}
 	panic("没有设置配置中心")
 }
