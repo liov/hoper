2c61d18500a7eda5b5f3e2268affce81b7aff2e8
贾一饼 <lby.i@qq.com>
2022-06-08 17:53:37 +0800

    fix: 日记时间处理

--- tools/server/timepill/timepill_test.go
+++ tools/server/timepill/timepill_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/actliboy/hoper/server/go/lib/utils/encoding/json"
 	"testing"
+	"time"
 )
 
 func TestJson(t *testing.T) {
@@ -15,4 +16,8 @@ func TestJson(t *testing.T) {
 func TestGetDiaryComments(t *testing.T) {
 	Token = "Basic bGJ5LmlAcXEuY29tOmxieTYwNA=="
 	fmt.Println(ApiService.GetDiaryComments(6817247))
+}
+
+func TestTimeParse(f *testing.T) {
+	fmt.Println(time.ParseInLocation("2006-01-02T15:04:05+08:00", "2010-03-18T13:03:48+08:00", time.Local))
 }
