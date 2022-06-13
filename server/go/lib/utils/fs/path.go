6000a8762fa0401a623ee6b1465976f9c91c66da
贾一饼 <lby.i@qq.com>
2022-06-13 11:38:04 +0800

    fix: 图片转换

--- server/go/lib/utils/fs/path.go
+++ server/go/lib/utils/fs/path.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 )
 
+// windows需要,由于linux的文件也要放到windows看,统一处理
 func PathClean(dir string) string {
 	dir = strings.ReplaceAll(dir, "<", "《")
 	dir = strings.ReplaceAll(dir, ">", "》")
