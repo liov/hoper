d4a23785dfa7efbec9c4bfe7921129605f6b8f99
贾一饼 <liov@hyx.com>
2022-06-30 17:14:00 +0800

    fix: 解决flutter重复日志,动态库报错(之前由于初始化了两个flutter engin)

--- client/desktop/linux/flutter/generated_plugins.cmake
+++ client/desktop/linux/flutter/generated_plugins.cmake
@@ -3,6 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  flutter_js
+  url_launcher_linux
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- client/desktop/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/desktop/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,30 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
+import connectivity_plus_macos
+import flutter_js
+import flutter_local_notifications
+import geolocator_apple
+import just_audio
+import package_info_plus_macos
+import path_provider_macos
+import share_plus_macos
+import shared_preferences_macos
+import sqflite
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FlutterJsPlugin.register(with: registry.registrar(forPlugin: "FlutterJsPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
