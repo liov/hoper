e215f61cf79fbe29ea89ea6590ee8792e6631749
贾一饼 <lby.i@qq.com>
2022-12-19 15:34:12 +0800

    fix: timepill

--- tools/server/clawer/timepill/timepill_test.go
+++ tools/server/clawer/timepill/timepill_test.go
@@ -3,6 +3,11 @@ package timepill
 import (
 	"fmt"
 	"github.com/liov/hoper/server/go/lib/utils/encoding/json/iterator"
+	"log"
+	surl "net/url"
+	"path"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 	"tools/clawer/timepill/model"
@@ -26,4 +31,19 @@ func TestTimeParse(f *testing.T) {
 func TestDownloadPic(t *testing.T) {
 	Conf.TimePill.PhotoPath = "D:/F/timepill"
 	DownloadPic(100774418, "http://s4.timepill.net/s/w640/photos/2022-08-24/b2s6hibqesmmq4opb8xbbgru8z4i3vwa.jpg", "2022-08-24 14:48:15")
+}
+
+func TestPicUrl(t *testing.T) {
+	date := "2010-03-17"
+	userId := 1
+	notebookId := 2
+	URL, _ := surl.Parse("http://s4.timepill.net/book_cover/0/15.jpg?v=1")
+	v := URL.Query().Get("v")
+	filepath := Conf.TimePill.PhotoPath
+	originFileName := path.Base(URL.Path)
+	originFileName = strings.TrimSuffix(originFileName, path.Ext(originFileName)) + "-v" + v + path.Ext(originFileName)
+	filepath += strings.Join([]string{filepath, "book_cover", date[:4], date[:7], strconv.Itoa(userId)}, "/")
+	filepath += "/" + strconv.Itoa(notebookId)
+	filepath += "/" + originFileName
+	log.Println(filepath)
 }
