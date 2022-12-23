ffd5a55ba19fa3f607c2aaa34c515f4d2dbd065c
贾一饼 <lby.i@qq.com>
2022-12-23 15:57:18 +0800

    fix: 包名变更

--- tools/server/clawer/bilibili/download/record.go
+++ tools/server/clawer/bilibili/download/record.go
@@ -3,7 +3,6 @@ package download
 import (
 	dbi "github.com/liov/hoper/server/go/lib/utils/dao/db/const"
 	"github.com/liov/hoper/server/go/lib_v2/utils/net/http/client/crawler"
-	"gorm.io/gorm"
 	"time"
 	"tools/clawer/bilibili/dao"
 )
@@ -11,7 +10,6 @@ import (
 func DownloadRecordVideo(engine *crawler.Engine) {
 	now := time.Now()
 	for {
-		gorm.Model{}
 		var videos []*Video
 		dao.Dao.Hoper.DB.Table(dao.TableNameVideo+" a").Select(`b.uid,b.aid,b.title,a.cid,a.page,a.part,a.record,b.pubdate pub_at,a.created_at`).Joins(`LEFT JOIN `+dao.TableNameView+" b ON a.aid = b.aid").Where(`b.record < 2  AND b.created_at < ? AND b.`+dbi.NotDeleted+`AND a.`+dbi.NotDeleted, now).Order(`b.created_at DESC`).Limit(100).Scan(&videos)
 		if len(videos) == 0 {
