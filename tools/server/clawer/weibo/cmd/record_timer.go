a7cc92145d81fd4817f0629099d775c8bb82c26f
贾一饼 <lby.i@qq.com>
2023-02-08 17:35:26 +0800

    fix: weibo record

--- tools/server/clawer/weibo/cmd/record_timer.go
+++ tools/server/clawer/weibo/cmd/record_timer.go
@@ -16,15 +16,13 @@ func main() {
 	defer initialize.Start(config.Conf, &dao.Dao)()
 
 	task1 := &listener.TimerTask{}
+	engine := crawler.NewEngine(config.Conf.Weibo.WorkCount).Timer(download.KindGet, time.Second)
+	engine.Run(download.GetUserFollowWeiboReq(""))
 	task1.Do = func(ctx context.Context) {
 		log.Println("task1 times", task1.Times)
-		engine := crawler.NewEngine(config.Conf.Weibo.WorkCount).Timer(download.KindGet, time.Second)
-
-		engine.Run(download.GetUserFollowWeiboReq(""))
+		engine.ReRun(download.GetUserFollowWeiboReq(""))
 	}
 	go task1.Timer(context.Background(), time.Minute)
-	engine := crawler.NewEngine(config.Conf.Weibo.WorkCount).Timer(download.KindGet, time.Second)
-	engine.Run(download.RecordUsersWeiboReq(config.Conf.Weibo.Users, true)...)
 	task := &listener.TimerTask{}
 	task.Do = func(ctx context.Context) {
 		log.Println("task times", task.Times)
