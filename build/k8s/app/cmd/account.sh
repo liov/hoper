c5016e488aeb5f753426c91ee584684b37028188
贾一饼 <lby.i@qq.com>
2023-04-28 19:42:59 +0800

    fix: account.sh

--- build/k8s/app/cmd/account.sh
+++ build/k8s/app/cmd/account.sh
@@ -10,8 +10,10 @@ elif [[ $1 == tot ]]; then
 	server=https://192.168.1.212:6443
 	cd certs/$cluster
 fi
+
+cd ../certs
 server=https://hoper.xyz:6443
-kubectl config set-cluster k8s --server=${server} --certificate-authority=certs/$1/ca.crt --embed-certs=true --kubeconfig=/root/.kube/config
-kubectl config set-credentials dev --client-certificate=certs/$1/dev.crt --client-key=certs/$1/dev.key --embed-certs=true --kubeconfig=/root/.kube/config
+kubectl config set-cluster k8s --server=$server --certificate-authority=$cluster/ca.crt --embed-certs=true --kubeconfig=/root/.kube/config
+kubectl config set-credentials dev --client-certificate=$cluster/dev.crt --client-key=$cluster/dev.key --embed-certs=true --kubeconfig=/root/.kube/config
 kubectl config set-context dev --cluster=k8s --user=dev --kubeconfig=/root/.kube/config
 kubectl config use-context dev --kubeconfig=/root/.kube/config
