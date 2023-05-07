da88a7bd77d118cc119fd95d73f38527a8f0a832
贾一饼 <lby.i@qq.com>
2023-05-07 19:16:39 +0800

    图片预览

--- client/uniapp/src/pages/moment/moment.vue
+++ client/uniapp/src/pages/moment/moment.vue
@@ -1,14 +1,14 @@
 <template>
     <view>
     <uni-list :border="false">
-        <view v-for="(moment,idx) in momentList" :key="idx"
-              :set=" users[idx] = userStore.getUser(moment.userId)">
+        <view v-for="(moment,midx) in momentList" :key="midx"
+              :set=" users[midx] = userStore.getUser(moment.userId)">
 
             <uni-card margin="2px 5px" padding="0">
                 <uni-list-item direction="column">
                     <!-- 自定义 header -->
                     <template v-slot:header>
-                        <uni-list-chat :avatar-circle="true" :title="users[idx].name" :avatar="staticDir+users[idx].avatarUrl"
+                        <uni-list-chat :avatar-circle="true" :title="users[midx].name" :avatar="staticDir+users[midx].avatarUrl"
                                        note="来自iPhone15 Pro Max" :time="moment.createdAt.slice(0,10)+' '+ moment.createdAt.slice(11)"></uni-list-chat>
                     </template>
                     <!-- 自定义 body -->
@@ -20,16 +20,16 @@
                             </view>
                             <view v-else-if="(images.length = 2) || (images.length = 4)">
                                 <uni-grid :column="2" :show-border="false" :square="false">
-                                    <uni-grid-item v-for="(img, index) in images" :index="index" :key="index">
+                                    <uni-grid-item v-for="(img, iidx) in images" :index="iidx" :key="iidx">
                                         <view class="grid-item-box">
-                                            <image :src="staticDir+img" :style="{width: '100%',height: 360/images.length+'px'}" mode="aspectFill" />
+                                            <image :src="staticDir+img" :style="{width: '100%',height: 360/images.length+'px'}" mode="aspectFill" @click="preview(midx,iidx)"/>
                                         </view>
                                     </uni-grid-item>
                                 </uni-grid>
                             </view>
                             <view v-else-if="images.length < 10">
                             <uni-grid :column="3" :show-border="false" :square="false">
-                                <uni-grid-item v-for="(img, index) in images" :index="index" :key="index">
+                                <uni-grid-item v-for="(img, iidx) in images" :index="iidx" :key="iidx">
                                     <view class="grid-item-box">
                                         <image :src="staticDir+img" style="width: 100%;height: 90px" mode="aspectFill" />
                                     </view>
@@ -38,9 +38,9 @@
                             </view>
                             <view v-else>
                                 <swiper class="swiper" :indicator-dots="true">
-                                    <swiper-item v-for="idx in Math.ceil(images.length/9)">
+                                    <swiper-item v-for="sidx in Math.ceil(images.length/9)">
                                         <uni-grid :column="3" :show-border="false" :square="false">
-                                            <uni-grid-item v-for="(img, index) in images.slice(9*(idx-1),9*idx)" :index="index" :key="index">
+                                            <uni-grid-item v-for="(img, iidx) in images.slice(9*(sidx-1),9*sidx)" :index="iidx" :key="iidx">
                                                 <view class="grid-item-box">
                                                     <image :src="staticDir+img" style="width: 100%;height: 90px" mode="aspectFill" />
                                                 </view>
@@ -60,11 +60,27 @@
         </view>
     </uni-list>
     <uni-load-more iconType="circle" :status="loadMoreStatus" @clickLoadMore="clickLoadMore"/>
+    <view class="popup-content">
+			<!-- 普通弹窗 -->
+			<uni-popup ref="popup" background-color="#fff">
+                <view class="popup-content">
+                    <uni-swiper-dot class="uni-swiper-dot-box" :info="previewImgs" :current="current" field="content" mode="round">
+                        <swiper style="width: 100%;height: 100%;" @change="change">
+                            <swiper-item v-for="(img ,iidx) in previewImgs" :key="iidx">
+                                <view class="swiper-item">
+                                    <image :src="staticDir+img" mode="aspectFit" style="width: 100vw;height: 100vh;"/>
+                                </view>
+                            </swiper-item>
+                        </swiper>
+                    </uni-swiper-dot>
+                </view>
+			</uni-popup>
+		</view>
     </view>
 </template>
 
 <script setup lang="ts">
-import Actions from "@/components/action"
+import Actions from "@/components/action.vue"
 import MomentService from "@/service/moment";
 import type {Ref} from "vue";
 import {ref} from "vue";
@@ -82,7 +98,6 @@ const listReq:PageRequest = {
 const users: User[] = [];
 const momentList: Ref<Moment[]> = ref([]);
 const loadMoreStatus = ref('more');
-
 
 getMomentList().then((res) => {
     momentList.value = momentList.value.concat(res.list)
@@ -103,12 +118,13 @@ onPullDownRefresh(async ()=> {
 
 
 onReachBottom(async ()=>{
+    if (loadMoreStatus.value == 'no-more') return;
     listReq.PageNo++;
     loadMoreStatus.value = 'loading';
     const res =  await getMomentList();
     if (res.list) {
         momentList.value = momentList.value.concat(res.list)
-        if( res.list < listReq.PageSize)  {
+        if( res.list.length < listReq.PageSize)  {
             loadMoreStatus.value = 'no-more';
             if (res.list.length == 0) listReq.PageNo--;
         } else {
@@ -116,14 +132,27 @@ onReachBottom(async ()=>{
         }
     }else {
         loadMoreStatus.value = 'no-more';
+        listReq.PageNo--;
     }
-})
+});
 
 function clickLoadMore(){
     if (loadMoreStatus.value == 'no-more') uni.showToast({title:"没有更多了",icon:"none"});
 }
 
-
+const popup = ref(null);
+const previewImgs:Ref<string[]> = ref([]);
+const current = ref(0);
+function preview(midx:number,iidx:number){
+    console.log(midx,iidx);
+    previewImgs.value = momentList.value[midx].images.split(',');
+    console.log(previewImgs.value);
+    current.value = iidx;
+    popup.value.open('center');
+}
+function change(e) {
+	current.value = e.detail.current
+};
 </script>
 
 <style scoped lang="scss">
@@ -146,4 +175,47 @@ function clickLoadMore(){
 :deep(.uni-list-item__container) {
   padding: 12px 15px 2px 15px;
 }
+
+    @mixin flex {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+	}
+
+	@mixin height {
+		/* #ifndef APP-NVUE */
+		height: 100%;
+		/* #endif */
+		/* #ifdef APP-NVUE */
+		flex: 1;
+		/* #endif */
+	}
+
+	.popup-content {
+        width: 100vw;
+		height: 100vh;
+	}
+
+    .uni-swiper-dot-box {
+			width: 100%;
+            height: 100%;
+			margin: 0 auto;
+			margin-top: 8px;
+		}
+/* 当屏幕宽度在 500px 以下时采用这种布局 */ 
+    @media screen and (max-width: 500px) {
+		.image {
+			width: 100%;
+		}
+	}
+/* 当屏幕宽度在 900px 以上时采用这种布局 */
+    @media  screen and (min-width: 900px) {
+    /* CSS 属性 */
+    }
+
+	.swiper-item {
+		text-align: center;
+	}
+
 </style>
