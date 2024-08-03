eadc450d48ecbdf3ab8cc64fb05b3e352221ea5f
贾一饼 <lby.i@qq.com>
2024-08-03 14:03:27 +0800

    update

--- gittag.sh
+++ gittag.sh
@@ -2,13 +2,13 @@ dir=$(cd $(dirname $0);pwd)
 cd $dir/thirdparty/protobuf
 go get github.com/hopeio/utils@main
 git add .
-git commit -m "fix: dependency upgrade"
+git commit -m "fix: upgrade dependency"
 git commit --amend --date="$(date -d '-10 hours' '+%Y-%m-%d %H:%M:%S')" --no-edit
 git tag "v0.0.0"
 
 cd $dir/thirdparty/context
 go get github.com/hopeio/utils@main
 git add .
-git commit -m "fix: dependency upgrade"
+git commit -m "fix: upgrade dependency"
 git commit --amend --date="$(date -d '-10 hours' '+%Y-%m-%d %H:%M:%S')" --no-edit
 git tag "v0.0.0"
