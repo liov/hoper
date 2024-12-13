db8e41b2065c4b681c7c3e29caaab85de3a7cfe0
贾一饼 <lby.i@qq.com>
2024-12-13 10:37:36 +0800

    feat: rfv thumbnail

--- server/rust/rfv/src/file.rs
+++ server/rust/rfv/src/file.rs
@@ -6,6 +6,10 @@ use axum::{
     response::{IntoResponse, Response},
     Json,
 };
+use ffmpeg_next as ffmpeg;
+use ffmpeg_next::decoder::Decoder;
+use ffmpeg_next::media::Type;
+use ffmpeg_next::software::scaling::Context;
 use image::{DynamicImage, GenericImageView, ImageFormat, ImageReader};
 use serde::{Deserialize, Serialize};
 use std::fs;
@@ -13,9 +17,6 @@ use std::io;
 use std::io::Cursor;
 use std::path::Path;
 use std::path::PathBuf;
-use ffmpeg_next as ffmpeg;
-use ffmpeg_next::decoder::Decoder;
-use ffmpeg_next::software::scaling::Context;
 use tracing::debug;
 
 #[derive(Deserialize)]
@@ -79,7 +80,7 @@ pub async fn file_thumbnail_handler(
     if !path_buf.exists() || !path_buf.is_file() {
         return Err((StatusCode::NOT_FOUND, "File not found".to_string()));
     }
-    const max_size: u32 = 256;
+    const MAX_SIZE: u32 = 256;
     if let Some(ext) = path_buf.extension() {
         if let Some(ext_str) = ext.to_str() {
             let lower_ext = ext_str.to_lowercase();
@@ -99,14 +100,14 @@ pub async fn file_thumbnail_handler(
                 let (width, height) = img.dimensions();
                 let thumbnail = if width > height {
                     img.resize(
-                        max_size,
-                        max_size * height / width,
+                        MAX_SIZE,
+                        MAX_SIZE * height / width,
                         image::imageops::FilterType::Triangle,
                     )
                 } else {
                     img.resize(
-                        max_size * width / height,
-                        max_size,
+                        MAX_SIZE * width / height,
+                        MAX_SIZE,
                         image::imageops::FilterType::Triangle,
                     )
                 };
@@ -125,57 +126,60 @@ pub async fn file_thumbnail_handler(
                     .into_response());
             } else if ["mp4", "flv", "avi", "rmvb"].contains(&lower_ext.as_str()) {
                 // 打开视频文件。
-                let mut context = match ffmpeg::format::input(&path_buf) {
+                let mut input = match ffmpeg::format::input(&path_buf) {
                     Ok(ctx) => ctx,
-                    Err(_) => return Err((StatusCode::NOT_FOUND, "Video file not found".to_string())),
+                    Err(_) => {
+                        return Err((StatusCode::NOT_FOUND, "Video file not found".to_string()))
+                    }
                 };
+                let context = match input.streams().best(Type::Video) {
+                    Some(ctx) => ctx,
+                    None => {
+                        return Err((StatusCode::NOT_FOUND, "Video file not found".to_string()))
+                    }
+                };
+                let video_stream_index = context.index();
                 // 提取第一帧作为缩略图。
-
+                // 获取解码器上下文。
+                let codec_context =
+                    ffmpeg::codec::context::Context::from_parameters(context.parameters()).unwrap();
+                let mut decoder = codec_context.decoder().video().unwrap();
+                // 准备接收帧的变量。
+                let mut key_frame_count = 0; // 用于计数关键帧的数量
+                let mut frame_count = 0; // 用于计数帧的数量
                 let mut frame = ffmpeg::frame::Video::empty();
-                for (stream, packet) in context.packets() {
-                    if stream.parameters().medium() == ffmpeg::media::Type::Video {
-                        debug!("stream: {:?}",stream);
-                        // 获取解码器上下文。
-                        let codec_context = ffmpeg::codec::context::Context::from_parameters(
-                            stream.parameters(),
-                        ).unwrap();
-                        let mut decoder = codec_context.decoder().video().unwrap();
-                        // 准备接收帧的变量。
-                        let mut key_frame_count = 0; // 用于计数关键帧的数量
-                        let mut frame_count = 0; // 用于计数帧的数量
+                for (stream, packet) in input.packets() {
+                    if stream.index() == video_stream_index {
+                        debug!("stream: {:?}", stream);
                         decoder.send_packet(&packet).unwrap();
-
-                         loop {
-                            match decoder.receive_frame(&mut frame) {
-                                Ok(_) => {
-                                    if frame.is_key() { // 检查当前帧是否是关键帧
-                                        key_frame_count += 1;
-                                        if key_frame_count == 3 { // 只需要第二个关键帧
-                                            break;
-                                        }
-                                    }
-                                    frame_count += 1;
-                                    if frame_count >= 10 {
-                                        break;
-                                    }
-                                },
-                                Err(ffmpeg::Error::Eof) => (),
-                                Err(err) => return Err((StatusCode::INTERNAL_SERVER_ERROR, format!("Error decoding video: {}", err))),
+                        while decoder.receive_frame(&mut frame).is_ok() {
+                            if frame.is_key() {
+                                // 检查当前帧是否是关键帧
+                                key_frame_count += 1;
+                                if key_frame_count == 3 {
+                                    // 只需要第二个关键帧
+                                    break;
+                                }
+                            }
+                            frame_count += 1;
+                            if frame_count >= 10 {
+                                break;
                             }
                         }
-                        break;
                     }
                 }
+                decoder.send_eof().unwrap();
                 // 将帧转换为 RGB 格式。
                 let mut rgb_frame = ffmpeg::frame::Video::empty();
-                let mut width = frame.width();
-                let mut height = frame.height();
-                if width > height {
-                    width=max_size;
-                    height=max_size*height/width;
+                let owidth = frame.width();
+                let oheight = frame.height();
+                let (mut width, mut height) = (owidth, oheight);
+                if owidth > oheight {
+                    width = MAX_SIZE;
+                    height = MAX_SIZE * oheight / owidth;
                 } else {
-                   height = max_size;
-                    width=max_size*width/height;
+                    height = MAX_SIZE;
+                    width = MAX_SIZE * owidth / oheight;
                 }
                 let mut scaler = Context::get(
                     frame.format(),
@@ -185,18 +189,18 @@ pub async fn file_thumbnail_handler(
                     width,
                     height,
                     ffmpeg::software::scaling::flag::Flags::BILINEAR,
-                ).unwrap();
+                )
+                .unwrap();
 
                 scaler.run(&frame, &mut rgb_frame).unwrap();
                 // 将 RGB 数据转换为图像。
-                let img = DynamicImage::ImageRgb8(image::RgbImage::from_raw(
-                    width,
-                    height,
-                    rgb_frame.data(0).to_vec(),
-                ).unwrap());
+                let img = DynamicImage::ImageRgb8(
+                    image::RgbImage::from_raw(width, height, rgb_frame.data(0).to_vec()).unwrap(),
+                );
                 // 将缩略图编码为 WebP 字节流。
                 let mut thumb_bytes: Vec<u8> = Vec::new();
-                img.write_to(&mut Cursor::new(&mut thumb_bytes), ImageFormat::WebP).unwrap();
+                img.write_to(&mut Cursor::new(&mut thumb_bytes), ImageFormat::WebP)
+                    .unwrap();
                 // 构建 HTTP 响应。
                 return Ok((
                     [
@@ -230,7 +234,10 @@ pub async fn file_thumbnail_handler(
             } else {
                 [
                     (header::CONTENT_TYPE, mime_type),
-                    (header::CACHE_CONTROL, "public, max-age=31536000".parse().unwrap()),
+                    (
+                        header::CACHE_CONTROL,
+                        "public, max-age=31536000".parse().unwrap(),
+                    ),
                 ]
             };
 
