b482232d3e3024744b1a9b1b6e61c1ad39e9318e
贾一饼 <lby.i@qq.com>
2025-01-09 12:58:17 +0800

    update

--- client/web/src/utils/service/woker.ts
+++
@@ -1,7 +0,0 @@
-export function startWorker(url: string): Worker {
-  if (typeof Worker !== "undefined") {
-    return new Worker(url);
-  } else {
-    throw new Error("抱歉，你的浏览器不支持 Web Workers...");
-  }
-}
--- client/web/src/utils/user.ts
+++ client/web/src/utils/user.ts
@@ -25,36 +25,3 @@ export function appendUserMap(
   }
   return map;
 }
-
-interface Obj<T> {
-  id: T;
-}
-
-export function appendObjMap<T extends Obj<any>>(
-  map: Map<number, T>,
-  objs: T[]
-): Map<number, T> {
-  for (const obj of objs) {
-    map.set(obj.id, obj);
-  }
-  return map;
-}
-
-export class ObjMap<K, V extends Obj<any>> {
-  _map = new Map<K, V>();
-
-  append(objs: V[]) {
-    for (const obj of objs) {
-      this._map.set(obj.id, obj);
-    }
-  }
-  get(id: K): Obj<any> {
-    return this._map.get(id)!;
-  }
-  set(id: K, v: V) {
-    this._map.set(id, v);
-  }
-  has(key: K): boolean{
-    return this._map.has(key)
-  }
-}
