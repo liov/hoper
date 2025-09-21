4c0cfcf17dd70091bf0b209ca246fc78e9672105
贾一饼 <lby.i@qq.com>
2025-09-21 19:05:18 +0800

    update

--- server/go/build.sh
+++ server/go/build.sh
@@ -1,4 +1,5 @@
 cd $(dirname $0) && pwd
+protogen go -e -w -v -p ../../proto -o protobuf
 GOOS=linux go build -trimpath -o ../../build/hoper main.go
 cd ../../build
 docker build -t jybl/hoper . && docker push jybl/hoper
