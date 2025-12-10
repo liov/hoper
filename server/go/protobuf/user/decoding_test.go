ef60f512effe1afcf9697f2d47d713fe09202afd
贾一饼 <lby.i@qq.com>
2025-12-10 19:39:54 +0800

    update

--- server/go/protobuf/user/decoding_test.go
+++ server/go/protobuf/user/decoding_test.go
@@ -1,9 +1,10 @@
 package user
 
 import (
-	"encoding/json"
+	"encoding/json/v2"
 	"testing"
 
+	"github.com/hopeio/protobuf/model"
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
@@ -19,4 +20,16 @@ func BenchmarkStdJson(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		json.Marshal(&gen)
 	}
+}
+
+func TestProtoJson(t *testing.T) {
+	gen := User{Basic: &model.Model{Id: 1}}
+	data, err := protojson.Marshal(&gen)
+	t.Log(string(data), err)
+}
+
+func TestJson(t *testing.T) {
+	gen := User{Basic: &model.Model{Id: 1}}
+	data, err := json.Marshal(&gen)
+	t.Log(string(data), err)
 }
